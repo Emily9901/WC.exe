2177fe2b233292c3662d938a5d7949da9e37c31d
Emily9901 <51350829+Emily9901@users.noreply.github.com>
2019-09-26 23:07:02 +0800

    Create 2.c

---
+++ test/T/2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>  //你我他
+int main(void)
+{
+    /*n
+    我和我的朋友n
+    你我他n
+
+    ？？？！！！n
+    */
+    int a,b,sum;c
+    a=3;/*   ghjjk  */
+    b=5;                 //n?c
+                         //c?n
+    sum=a+b;
+    printf("sum=%d\n",sum);
+    /*op*/
+    return 0;
+    return 0; }  //这里我又写了一遍
