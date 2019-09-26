682ea55422a1a24eb24e0a64c74447e44ecae256
Emily9901 <51350829+Emily9901@users.noreply.github.com>
2019-09-26 23:10:11 +0800

    Create 15.c

---
+++ test/15.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+ void main()
+ {
+  int c[11] ={0,1,2,3,4,5,6,7,8,9,10};
+  printHeap(c)  ;
+ }
+
+ void printHeap(int *p)
+     {/*fghg*/
+
+      int i,j,k;
+      printf("ggh");
+      for(i=1,j=1;i<=10;i++)
+      {
+       for(k=(10/2+1)-j;k>0;k--)
+       printf(" " );
+       printf("%d  ",p[i]);
+       if(i==(2^j)-1)
+       {
+       printf("\n");
+       j++;
+       }
+       }
+     }
