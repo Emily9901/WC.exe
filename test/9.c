ae960eaf51461e46f61c3b870800ed71e2135c67
Emily9901 <51350829+Emily9901@users.noreply.github.com>
2019-09-26 23:09:08 +0800

    Create 9.c

---
+++ test/9.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+ int Max(int x,int y);
+ int a,b,c;
+mh/*
+ scanf("%d,%d",&a,&b);
+
+ c=Max(a,b);
+ printf("The max is %d\n",c);
+ return 0;
+*/
+}
+int Max(int x,int y)
+{
+  int z;
+  if(x>y)z=x;
+  else z=y;
+  return (z);
+}
